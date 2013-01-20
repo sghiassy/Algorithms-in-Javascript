8d4bf184673fc6af9941bc963a5fede9d7dc00f4
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2013-01-19 22:51:20 -0800

    Added more unit tests

--- spec/StackSpec.js
+++ spec/StackSpec.js
@@ -76,4 +76,19 @@ describe("Stack", function() {
 		expect(stack.pop()).toEqual(undefined);
 		expect(stack.pop()).toEqual(undefined);
 	});
+	
+	it("it can do math expessions: 5 * (((9 + 8) * (4 * 6)) + 7) = 2075", function() {
+		stack.push(5);
+		stack.push(9);
+		stack.push(8);
+		stack.push(stack.pop() + stack.pop());
+		stack.push(4);
+		stack.push(6);
+		stack.push(stack.pop() * stack.pop());
+		stack.push(stack.pop() * stack.pop());
+		stack.push(7);
+		stack.push(stack.pop() + stack.pop());
+		stack.push(stack.pop() * stack.pop());
+		expect(stack.pop()).toEqual(2075);
+	});
 });
